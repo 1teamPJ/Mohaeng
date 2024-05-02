ec7f3ef38c756252444ff825d356738d8ba51870
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-05-02 14:57:14 +0900

    recommend랑 header를 layout으로 묶고 testpage를 따로 설정함

--- mohanengpj/mohanengpj/src/App.js
+++ mohanengpj/mohanengpj/src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom' 
 import Header from './Header';
+import Recommend from './Recommend';
+import Testpage from './testpage';
+import Layout from './layouts/Layout';
 
 
 function App() {
   return (
     <>
-      <Header/>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Layout />}/>
+        
+        <Route path="/testpage" element={ <Testpage />}/>
+        
+      </Routes>
+    </BrowserRouter>
+      
     </>
   );
 }
