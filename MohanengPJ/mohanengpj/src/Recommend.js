8d55c75e8edfe2b766cbcb315bd579c8ffb86453
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-05-02 13:25:56 +0900

    여행 추천을 따로 뺌

---
+++ MohanengPJ/mohanengpj/src/Recommend.js
@@ -0,0 +1,46 @@
+import './recommend.css'
+import noneimg1 from './img/noneimg1.png';
+import noneimg2 from './img/noneimg2.png';
+import noneimg3 from './img/noneimg3.png';
+
+const triprm = [
+    {
+        id : 1,
+        img : {noneimg1}
+    },
+    {
+        id : 2,
+        img : {noneimg2}
+    },
+    {
+        id : 3,
+        img : {noneimg3}
+    }
+]
+
+function Recommend({ user }){
+    return(
+        <>
+            <div class="recommendTrip">
+                <h1 class="">이런 여행은 어떠세요?</h1>
+                <div class="rcimg">
+                    {/* <Recommend user={triprm[0].img}/> */}
+                    {/* <img src={noneimg1}></img>
+                    <img src={noneimg2}></img>
+                    <img src={noneimg3}></img>
+                    <img src={noneimg1}></img>
+                    <img src={noneimg2}></img>
+                    <img src={noneimg3}></img>
+                    <img src={noneimg1}></img>
+                    <img src={noneimg2}></img>
+                    <img src={noneimg3}></img>
+                    <img src={noneimg3}></img>
+                    <img src={noneimg3}></img>
+                    <img src={noneimg3}></img> */}
+                    <button>버튼</button>
+                </div>
+            </div>
+        </>
+    );
+}
+export default Recommend;
