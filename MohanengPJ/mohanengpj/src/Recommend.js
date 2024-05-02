07019e5af57f31efd0ad3838a34b72cf6a3d8db9
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-05-02 16:16:25 +0900

    #10 추천 기능

--- MohanengPJ/mohanengpj/src/Recommend.js
+++ MohanengPJ/mohanengpj/src/Recommend.js
@@ -3,45 +3,57 @@ import noneimg1 from './img/noneimg1.png';
 import noneimg2 from './img/noneimg2.png';
 import noneimg3 from './img/noneimg3.png';
 import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
-const triprm = [
+/* const triprm = [
     {
         id : 1,
-        img : {noneimg1}
+        imgSrc: noneimg1
     },
     {
         id : 2,
-        img : {noneimg2}
+        imgSrc: noneimg2
     },
     {
         id : 3,
-        img : {noneimg3}
+        imgSrc: noneimg3
     }
-]
+] */
 
-function Recommend({ user }){
-    return(
-        <>
-            <div class="recommendTrip">
-                <h1 class="">이런 여행은 어떠세요?</h1>
-                <div class="rcimg">
-                    {/* <Recommend user={triprm[0].img}/> */}
-                    {/* <img src={noneimg1}></img>
-                    <img src={noneimg2}></img>
-                    <img src={noneimg3}></img>
-                    <img src={noneimg1}></img>
-                    <img src={noneimg2}></img>
-                    <img src={noneimg3}></img>
-                    <img src={noneimg1}></img>
-                    <img src={noneimg2}></img>
-                    <img src={noneimg3}></img>
-                    <img src={noneimg3}></img>
-                    <img src={noneimg3}></img>
-                    <img src={noneimg3}></img> */}
-                    <button><NavLink to="/testpage" className="noneunderline">버튼</NavLink></button>
+    const allImgs = [
+        noneimg1, noneimg2, noneimg3, noneimg1, noneimg2, noneimg2, noneimg2, noneimg3, noneimg3, noneimg3, noneimg3, noneimg3, noneimg3
+    ];
+  
+
+    function Recommend({ user }){
+        const [randomImgs, setRandomImgs] = useState([]);
+
+        useEffect(() => {
+            const chooseRandomImgs = () => {
+              const shuffledImgs = allImgs.sort(() => 0.5 - Math.random());
+              setRandomImgs(shuffledImgs.slice(0, 3));
+            };
+        
+            chooseRandomImgs();
+          }, []);
+
+        return(
+            <>
+                <div class="recommendTrip">
+                    <h1 class="">이런 여행은 어떠세요?</h1>
+                    <div class="rcimg">
+                        {
+                        /* randomImgs.map((item) => (
+                            <img key={item.id} src={item.imgSrc} alt={`이미지 ${item.id}`} />
+                        )) */
+                        randomImgs.map((imgSrc, index) => (
+                            <img key={index} src={imgSrc} alt={`Recommended ${index}`} />
+                          ))
+                        }
+                        <button><NavLink to="/testpage" className="noneunderline">버튼</NavLink></button>
+                    </div>
                 </div>
-            </div>
-        </>
-    );
-}
-export default Recommend;
+            </>
+        );
+    }
+    export default Recommend;
