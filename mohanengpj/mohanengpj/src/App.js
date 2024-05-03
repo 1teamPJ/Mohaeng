5806bc98f7a8c446aa4152d1447a353bd1f1dd3e
yena <a75372656@gmail.com>
2024-05-03 18:14:45 +0900

    travelboard + dev

--- mohanengpj/mohanengpj/src/App.js
+++ mohanengpj/mohanengpj/src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './pages/Header';
 import Layout from './layouts/Layout';
 import CheckBoard from './pages/CheckBoard';
+// import TravelRecommendations from './pages/TravelRecommendations';
+import TravelRecommendations from './pages/TravelRecommandations';
+import './css/travelstyle.css';
+
 function App() {
   return (
     <>
@@ -11,6 +15,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={ <Header /> } />
           <Route path="checkboard" element={<CheckBoard/>}/>
+          <Route path="travelboard" element={<TravelRecommendations/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
