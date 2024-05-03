67c56e1eaa3ec6e49b20e70b2c882fa42bda0ba9
yena <a75372656@gmail.com>
2024-05-03 17:45:02 +0900

    저장
    저장

--- MohanengPJ/mohanengpj/src/TravelRecommandations.js
+++ MohanengPJ/mohanengpj/src/TravelRecommandations.js
@@ -4,6 +4,12 @@ import busanImage from './images/busan.jpg';
 import jejuImage from './images/jeju.jpg';
 import gangneungImage from './images/gangneung.jpg';
 import gyeongjuImage from './images/gyeongju.jpg';
+import jeonjuImage from './images/jeonju.jpg';
+import incheonImage from './images/incheon.jpg';
+import yeosuImage from './images/yeosu.jpg';
+import chuncheonImage from './images/chuncheon.jpg';
+
+
 
 
 function TravelRecommendations(){
@@ -47,6 +53,46 @@ function TravelRecommendations(){
             image: gyeongjuImage,
             region: '경주',
             rank: 5
+        },
+        { 
+            id: 6, 
+            title: '전주', 
+            content: '전주에 위치한 한옥마을은 전통적인 한국 문화를 경험할 수 있는 곳으로 유명합니다.',
+            image: jeonjuImage,
+            region: '전주',
+            rank: 6
+        },
+        { 
+            id: 7, 
+            title: '인천', 
+            content: '인천은 현대적인 도시 풍경과 역사적인 유적지를 함께 감상할 수 있는 매력적인 여행지입니다.',
+            image: incheonImage,
+            region: '인천',
+            rank: 7
+        },
+        { 
+            id: 8, 
+            title: '여수', 
+            content: '여수는 한국의 남해안에 위치한 아름다운 도시로, 특히 "바다의 도시"로 불립니다. ',
+            image: yeosuImage,
+            region: '여수',
+            rank: 8
+        },
+        { 
+            id: 9, 
+            title: '춘천', 
+            content: '춘천은 한국의 강원도에 위치한 아름다운 도시로, 자연 경관과 맛있는 음식, 문화적인 명소로 유명합니다. ',
+            image: chuncheonImage,
+            region: '춘천',
+            rank: 9
+        },
+        { 
+            id: 10, 
+            title: '대구', 
+            content: '대구는 한국의 대표적인 도시 중 하나로, 역사와 문화, 먹거리가 풍부한 도시입니다. ',
+            image: gyeongjuImage,
+            region: '대구',
+            rank: 10
         }
     ]);
     const [searchTerm, setSearchTerm] = useState('');
@@ -63,8 +109,8 @@ function TravelRecommendations(){
 
     return (
         <div>
-            <h1>여행 추천</h1>
-            
+            <h1>여행 추천 순위</h1>
+
             <input 
                 type="text" 
                 placeholder="제목, 지역, 순위를 입력하세요" 
