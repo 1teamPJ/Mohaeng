0af327c9093c9d4523faba7cb8ef5fa8094b2d13
Realyunjae <129252557+Realyunjae@users.noreply.github.com>
2024-05-03 17:52:11 +0900

    #19 CBboard main 연결

---
+++ MohanengPJ/mohanengpj/src/pages/CBheader.js
@@ -0,0 +1,13 @@
+
+function CBheader() {
+
+
+    return(
+        <div div class ="CBheader">
+        <h1> 이번 여행 당신의 취향은?</h1>
+        </div>
+    );
+}
+export default CBheader;
+
+
--- mohanengpj/mohanengpj/src/App.js
+++ mohanengpj/mohanengpj/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom' 
 import Header from './pages/Header';
 import Layout from './layouts/Layout';
-import CheckBoard from './pages/CheckBoard';
+import CbLayout from './layouts/CbLayout';
+
 function App() {
   return (
     <>
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={ <Header /> } />
-          <Route path="checkboard" element={<CheckBoard/>}/>
+          <Route path="checkboard" element={<CbLayout/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
