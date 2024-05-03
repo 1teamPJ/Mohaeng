1e5408e08f600fb9c26183e2028f2ae239fc5142
nayeon <nayeon08911@gmail.com>
2024-05-03 17:54:19 +0900

    11
    11

--- mohanengpj/mohanengpj/src/App.js
+++ mohanengpj/mohanengpj/src/App.js
@@ -2,6 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './pages/Header';
 import Layout from './layouts/Layout';
 import CheckBoard from './pages/CheckBoard';
+
+
+import FeedBoard from './pages/FeedBoard';
+import './css/feedstyle.css';
+
+
 function App() {
   return (
     <>
@@ -11,6 +17,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={ <Header /> } />
           <Route path="checkboard" element={<CheckBoard/>}/>
+          <Route path="feedboard" element={<FeedBoard/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
