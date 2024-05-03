f2c29c7c4dcda4df3f4eed154ff9b361e32935e9
ohdyo <smart5572@naver.com>
2024-05-03 16:59:09 +0900

    Route설정 완료

--- MohanengPJ/mohanengpj/src/components/Front.js
+++ MohanengPJ/mohanengpj/src/components/Front.js
@@ -5,7 +5,7 @@ function Front(){
     return(
         <div>
             <img src={logo} alt="로고 없음" height="100"/>
-            <NavLink to="/"></NavLink>
+            <NavLink to="/">메인</NavLink>
             <NavLink to="/checkboard">체크보드</NavLink>
             <NavLink to="/feedboard">피드보드</NavLink>
             <NavLink to="/travelboard">여행보드</NavLink>
---
+++ MohanengPJ/mohanengpj/src/pages/CheckBoard.js
@@ -0,0 +1,11 @@
+function CheckBoard(){
+    return(
+        <>
+            <div>
+                <h1>테스트체크보드</h1>
+            </div>
+        </>
+    );
+}
+
+export default CheckBoard;
