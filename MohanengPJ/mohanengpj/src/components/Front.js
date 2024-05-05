c7698c4a41eedcebe066702ee0980ed5f2fb181b
Ahnhyojun <hyojunahn111@naver.com>
2024-05-05 18:54:04 +0900

    #35 추천 이미지 변경, 이미지 밑에 버튼 클릭하여 순위로 이동되게 변경

--- MohanengPJ/mohanengpj/src/components/Front.js
+++ MohanengPJ/mohanengpj/src/components/Front.js
@@ -1,5 +1,5 @@
 // User 컴포넌트 코드
-import logo from '../img/nologo.png';
+import logo from '../img/mohaenglogo.png';
 import { NavLink } from 'react-router-dom';
 import styles from './Front.module.css'; // CSS 모듈을 import하여 styles 객체로 사용합니다
 
--- MohanengPJ/mohanengpj/src/pages/Recommend.js
+++ MohanengPJ/mohanengpj/src/pages/Recommend.js
@@ -1,16 +1,26 @@
-import './recommend.css'; // CSS 파일을 import하여 스타일을 적용합니다
-import noneimg1 from '../img/noneimg1.png'; // 이미지를 import 합니다
+import './recommend.css';
+/* import noneimg1 from '../img/noneimg1.png';
 import noneimg2 from '../img/noneimg2.png';
-import noneimg3 from '../img/noneimg3.png';
-import { NavLink } from 'react-router-dom';
+import noneimg3 from '../img/noneimg3.png'; */
+import seoulImage from '../img/seoul.jpg';
+import busanImage from '../img/busan.jpg';
+import jejuImage from '../img/jeju.jpg';
+import gangneungImage from '../img/gangneung.jpg';
+import gyeongjuImage from '../img/gyeongju.jpg';
+import jeonjuImage from '../img/jeonju.jpg';
+import incheonImage from '../img/incheon.jpg';
+import yeosuImage from '../img/yeosu.jpg';
+import chuncheonImage from '../img/chuncheon.jpg';
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const allImgs = [
-    noneimg1, noneimg2, noneimg3, noneimg1, noneimg2, noneimg2, noneimg2, noneimg3, noneimg3, noneimg3, noneimg3, noneimg3, noneimg3
+    seoulImage, busanImage, jejuImage, gangneungImage, gyeongjuImage, jeonjuImage, incheonImage, yeosuImage, chuncheonImage
 ];
 
 function Recommend({ user }) {
     const [randomImgs, setRandomImgs] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const chooseRandomImgs = () => {
@@ -21,15 +31,20 @@ function Recommend({ user }) {
         chooseRandomImgs();
     }, []);
 
+    const handleClick = () => {
+        navigate("/travelboard");
+        window.scrollTo(0, 0);
+    };
+
     return (
         <div className="recommendTrip">
             <h1>이런 여행은 어떠세요?</h1>
             <div className="rcimg">
                 {randomImgs.map((imgSrc, index) => (
                     <img key={index} src={imgSrc} alt={`Recommended ${index}`} />
-                ))}
-               
+                ))}  
             </div>
+            <button className="gotravelbt" onClick={handleClick}><h3>추천 여행지 보러가기</h3></button>
         </div>
     );
 }
